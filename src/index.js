776c1323b27864f925a9a1857712637304b5ac70
Miron Fissenko <mirfees@gmail.com>
2026-07-07 00:24:34 +0500

    FIX: опечатка в constructor

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class Analytics {
         this.eventsTriggeredLast = new Set();
         this.settings = Object.assign(defaultSettings, customSettings);
         this.forms = this.getForms();
-        this.settings.hiddenFields["Preffered_Contact_Language"].value = this.language;
+        this.settings.hiddenFields["Preferred_Contact_Language"].value = this.language;
         this.tgLinks = document.querySelectorAll(`a[href="${this.settings.tgBaseLink}"]`);
         Analytics.instance = this;
 
