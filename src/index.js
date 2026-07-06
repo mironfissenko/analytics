0bb6e7500cdb46d6599628724c76f19af538015c
Miron Fissenko <mirfees@gmail.com>
2026-07-06 23:07:39 +0500

    RFC: index.js

--- src/index.js
+++ src/index.js
@@ -20,6 +20,30 @@ class Analytics {
         return this;
     }
 
+    getValidateField(name) {
+        return this.settings.validateFields[name];
+    }
+
+    setValidateField(name, errorType = ["custom", "custom"]) {
+        this.settings.validateFields[name] = {"error_type": error_type};
+    }
+
+    removeValidateField(name) {
+        delete this.settings.validateFields[name];
+    }
+
+    getHiddenField(name) {
+        return this.settings.hiddenFields[name];
+    }
+
+    setHiddenField(name, type = "text", value = "") {
+        this.settings.hiddenFields[name] = {value, type};
+    }
+
+    removeHiddenField(name) {
+        delete this.settings.hiddenFields[name];
+    }
+
     _constructSendPulseLink (pulseValues = this.settings.tgPulseValues, urlBase = "https://tg.pulse.is/", botName = this.settings.tgBotName, pulseStart = this.settings.tgSendPulseStart) {
         let link = urlBase + botName + "?start=" + pulseStart;
 
